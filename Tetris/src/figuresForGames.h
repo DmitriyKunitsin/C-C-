74a521de09706bec3b3c4be504322498ae8c262d
DmitriyKunitsin <DmitriyKunitsin>
2024-04-05 10:19:13 +0700

    add TZ and header for figures

---
+++ Tetris/src/figuresForGames.h
@@ -0,0 +1,9 @@
+#ifndef FIGURES_FOR_GAMES_H
+#define FIGURES_FOR_GAMES_H
+
+int figure_one[4][4] = {{0 , 1 , 0 , 0},
+                     {0 , 1, 0 , 0},
+                     {0 , 1 , 0 , 0},
+                     {0 , 0 , 0 , 0}};
+
+#endif //FIGURES_FOR_GAMES_H
