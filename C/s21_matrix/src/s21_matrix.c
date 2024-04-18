6e76af7be25f9ad4141bc5de60f98a98273acadd
DmitriyKunitsin <DmitriyKunitsin>
2024-04-18 14:32:34 +0700

    add new func s21_eq_matrix

--- C/s21_matrix/src/s21_matrix.c
+++ C/s21_matrix/src/s21_matrix.c
@@ -42,3 +42,20 @@ void s21_remove_matrix(matrix_t *A) {
     A->matrix = NULL;
   }
 }
+
+int s21_eq_matrix(matrix_t *A, matrix_t *B) {
+  int error = SUCCESS;
+  if ((A->rows == B->rows) && (A->columns == B->columns)) {
+    for (int i = 0; i < A->rows; ++i) {
+      for (int k = 0; k < A->columns; ++k) {
+        if (!isclose(A->matrix[i][k], B->matrix[i][k], 1e-7)) {
+          error = FAILURE;
+        }
+      }
+    }
+  } else {
+    error = FAILURE;
+  }
+
+  return error;
+}
--- C/s21_matrix/src/s21_matrix.h
+++ C/s21_matrix/src/s21_matrix.h
@@ -2,11 +2,14 @@
 #define S21_MATRIX_H
 
 #include <stdlib.h>
+#include <math.h>
 
 #define FAILED_MATRIX 2
 #define INCORRECT_MATRIX 1
 #define OK 0
 
+#define SUCCESS 1
+#define FAILURE 0
 
 typedef struct matrix_struct {
     double** matrix;
@@ -16,5 +19,6 @@ typedef struct matrix_struct {
 
 int s21_create_matrix(int rows, int columns, matrix_t *result);
 void s21_remove_matrix(matrix_t *A);
+int s21_eq_matrix(matrix_t *A, matrix_t *B);
 
 #endif
