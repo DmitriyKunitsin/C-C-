9956c020020ca720af4329ba250c3a53a4c459ca
DmitriyKunitsin <DmitriyKunitsin>
2024-04-18 14:41:05 +0700

    add new two func sub and sum matrix

--- C/s21_matrix/src/s21_matrix.c
+++ C/s21_matrix/src/s21_matrix.c
@@ -59,3 +59,31 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
 
   return error;
 }
+
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+  int error = SUCCESS;
+  if ((A->rows == B->rows) && (A->columns == B->columns)) {
+    for (int i = 0; i < A->rows; ++i) {
+      for (int k = 0; k < A->columns; ++k) {
+        result->matrix[i][k] = A->matrix[i][k] + B->matrix[i][k];
+      }
+    }
+  } else {
+    error = FAILURE;
+  }
+  return error;
+}
+
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+  int error = SUCCESS;
+  if ((A->rows == B->rows) && (A->columns == B->columns)) {
+    for (int i = 0; i < A->rows; ++i) {
+      for (int k = 0; k < A->columns; ++k) {
+        result->matrix[i][k] = A->matrix[i][k] - B->matrix[i][k];
+      }
+    }
+  } else {
+    error = FAILURE;
+  }
+  return error;
+}
--- C/s21_matrix/src/s21_matrix.h
+++ C/s21_matrix/src/s21_matrix.h
@@ -16,9 +16,15 @@ typedef struct matrix_struct {
     int rows;
     int columns;
 } matrix_t;
-
+/*Создание матрицы*/
 int s21_create_matrix(int rows, int columns, matrix_t *result);
+/*Очищение матрицы*/
 void s21_remove_matrix(matrix_t *A);
+/*Сравнение*/
 int s21_eq_matrix(matrix_t *A, matrix_t *B);
+/*Вычитание*/
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+/*Сложение*/
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 
 #endif
